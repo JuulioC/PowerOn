162d48ebde3864d6d827618e6c67cecba6364014
Júlio Cezar <j.cezarcc@gmail.com>
2025-07-04 01:27:30 -0300

    - Implementacao de Atendimento, somente novo atendimento Obs, precisam de ajustes par a layout de pacotes, algumas informacoes visuais estao deslocadas e nao estao encaixando.

---
+++ PowerOn/Controllers/AtendimentoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks; // Usaremos para futuras chamadas de API
+
+namespace PowerOn.Controllers
+{
+    // Garante que apenas usuários logados possam acessar
+    // [Authorize] 
+    public class AtendimentoController : Controller
+    {
+        // GET: /Atendimento/Novo
+        // Vamos usar uma ação chamada 'Novo' para representar a página de novo atendimento.
+        [HttpGet]
+        public IActionResult Novo()
+        {
+            // Esta linha diz ao ASP.NET para procurar o arquivo:
+            // /Views/Atendimento/Novo.cshtml
+            return View();
+        }
+
+        // Futuramente, aqui entrarão as outras ações, como:
+        // - BuscarCliente(string termo)
+        // - GerarOrdemDeServico(...)
+    }
+}
